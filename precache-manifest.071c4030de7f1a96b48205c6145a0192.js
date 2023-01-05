e3129fd1650d4a7840a149a5ba2f7997e6b25c58
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-01-05 11:12:48 +0000

    Deploy renato-bohler/react-web-worker to github.com/renato-bohler/react-web-worker.git:gh-pages

---
+++ precache-manifest.071c4030de7f1a96b48205c6145a0192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770d86979cb06f6c8ebdca962ed73057",
+    "url": "https://renato-bohler.github.io/react-web-worker/index.html"
+  },
+  {
+    "revision": "c848195eadefd7ede2e5",
+    "url": "https://renato-bohler.github.io/react-web-worker/static/css/main.be196e3f.chunk.css"
+  },
+  {
+    "revision": "210e32133928a0f1c9b8",
+    "url": "https://renato-bohler.github.io/react-web-worker/static/js/2.704ab889.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "https://renato-bohler.github.io/react-web-worker/static/js/2.704ab889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c848195eadefd7ede2e5",
+    "url": "https://renato-bohler.github.io/react-web-worker/static/js/main.9e57f871.chunk.js"
+  },
+  {
+    "revision": "f43e094264830ff3a9d2",
+    "url": "https://renato-bohler.github.io/react-web-worker/static/js/runtime-main.6f4ffe35.js"
+  }
+]);
